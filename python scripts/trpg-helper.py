4c74f35c9456be3fea15eb747edcfb46b12540a8
Jeyu Chun <67189530+jcxmt125@users.noreply.github.com>
2024-06-22 19:25:24 +0900

    DiceNotation + JSON support
    DiceNotation is WIP

--- python scripts/trpg-helper.py
+++ python scripts/trpg-helper.py
@@ -1,4 +1,5 @@
 import json
+import random
 
 #This script is a WIP!
 
@@ -21,7 +22,30 @@ def stat_to_delta(statvalue):
 def roll_dice_from_notation(dice_notation):
     notation_list = dice_notation.split()
 
+    number_list = []
+
+    for i in notation_list:
+        if "d" in i:
+            dicevalue = i.split("d")
+            number_list.append(dicevalue[0]*random.randint(1,dicevalue[2]))
+        else:
+            number_list.append(i)
     
+    result = 0
+
+    for i in number_list:
+        symbol = 1
+        try:
+            if symbol:
+                result += int(i)
+            else:
+                result -= int(i)
+        except:
+            if i == "+":
+                symbol = 1
+            elif i == "-":
+                symbol = 0
+        
 
 def make_savefile_cleric_interactive():
     character_name = input("What is the name of the character? ")
@@ -35,31 +59,35 @@ def make_savefile_cleric_interactive():
         "charisma":int(input("Input charisma: "))
     }
     
-    max_hp = stats["constitution"] + 8
-    
     base_attack = input("Input base attack in standard dice notation (ex. d4, d5 + 2, ...): ")
 
-    max_carry = 10 + stat_to_delta(stats["strength"])
-
     print("Let's start adding the inventory!")
-
-    carry = 0
     
     armor = [input("Armor name: "), int(input("Armor stat modifier: ")), int(input("Weight of armor: ")), input("Any other notes? (NL): ")]
 
-    armor_modifier = armor[1]
-
-    carry += armor[2]
-
     weapon = [input("Weapon name: "), input("Range of weapon: "), int(input("Weight of weapon: ")), input("Any other notes? (NL): ")]
 
-    carry += weapon[2]
-
     holy_artifact = [input("Name of the artifact: "), int(input("Weight of artifact: ")), input("Any other notes? (NL): ")]
-    
-    carry += holy_artifact[1]
+
+    money = 0
+
+    food = 5
+
+    equipment = 5
 
     inventory_misc = {}
 
+    with open(input("File name to save to: "),"w",encoding="UTF-8") as f:
+        
+        charFile = {
+            "stats":stats,
+            "max_hp":stats["constitution"] + 8,
+            "max_carry":10 + stat_to_delta(stats["strength"]),
+            "current_carry":armor[2] + weapon[2] + holy_artifact[1] + round(money/100) + round(food/5) + round(equipment/5),
+            "base_attack":base_attack
+        }
+
+        json.dump(charFile, f)
+
 if __name__ == "__main__":
-    make_savefile_cleric_interactive()
+    roll_dice_from_notation(input("Dice Notation: "))
