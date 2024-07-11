fc5d14c445d0ef5fcd568f62320cbfba7af44f5a
Jeyu Chun <67189530+jcxmt125@users.noreply.github.com>
2024-07-11 22:15:42 +0900

    Multimodal but now with support for more images

--- python scripts/gemini-api-multimodal.py
+++ python scripts/gemini-api-multimodal.py
@@ -9,9 +9,17 @@ GEMINI_API_KEY = os.getenv('GEMINI_API_KEY')
 
 genai.configure(api_key=GEMINI_API_KEY)
 
-image = PIL.Image.open(input("Drag file here: "))
+image = []
 
-construct = [{'role':'user','parts': ["What is in this image?", image]}]
+while True:
+    
+    filePath = input("Drag file here: ")
+    
+    if filePath == "": break
+
+    image.append(PIL.Image.open(filePath))
+
+construct = [{'role':'user','parts': ["What is in these images?"] + image}]
 
 model = genai.GenerativeModel('gemini-1.5-flash-latest')
 
